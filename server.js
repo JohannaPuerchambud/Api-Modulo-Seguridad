e48c20a986af7c625e405f3fa39584fed6d397c7
RELIC7663 <alexispepinosa28@gmail.com>
2024-06-16 19:41:24 -0500

    comi 1

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const auditoriaRoutes = require('./src/routes/auditoriaRoutes');
 const moduleRoutes = require('./src/routes/moduleRoutes');
+const functionRoutes = require('./src/routes/functionRoutes');
 const db = require('./src/models/db'); // Archivo donde se define la conexión a la base de datos
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(cors());
 // Rutas
 app.use('/api', auditoriaRoutes);
 app.use('/api', moduleRoutes);
+app.use('/api', functionRoutes);
 // Puedes añadir más rutas según tus necesidades
 
 app.listen(port, () => {
