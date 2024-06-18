6e762c38d22403716dd6df4a7cbc595bc635942d
JohannaPuerchambud <jepuerchambudp@utn.edu.ec>
2024-06-18 11:15:24 -0500

    conexion local

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,30 @@
-// models/db.js
-
 const { Pool } = require('pg');
 
-// Configurar la conexión a la base de datos utilizando la variable de entorno DATABASE_URL
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  user: 'postgres', // Usuario de la base de datos
+  host: 'localhost', // Dirección del servidor de la base de datos
+  database: 'moduloSeguridad', // Nombre de la base de datos
+  password: '1998', // Contraseña del usuario de la base de datos
+  port: 5432, // Puerto por defecto de PostgreSQL
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
+
+
+// // models/db.js
+
+// const { Pool } = require('pg');
+
+// // Configurar la conexión a la base de datos utilizando la variable de entorno DATABASE_URL
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// };
