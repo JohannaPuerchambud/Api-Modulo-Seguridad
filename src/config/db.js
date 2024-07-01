417da37ba0aa621a7807debedc5d427faec57dab
JohannaPuerchambud <jepuerchambudp@utn.edu.ec>
2024-07-01 17:51:32 -0500

    conexion

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ const pool = new Pool({
   host: process.env.DB_HOST, // Dirección del servidor de la base de datos desde la variable de entorno
   database: process.env.DB_NAME, // Nombre de la base de datos desde la variable de entorno
   password: String(process.env.DB_PASSWORD), // Contraseña del usuario de la base de datos desde la variable de entorno
+  port: process.env.DB_PORT, // Puerto desde la variable de entorno
 });
 
 module.exports = {
