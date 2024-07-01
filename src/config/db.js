3b4bfd5829c0fbcd3c21a969516bfcf988ed69e8
JohannaPuerchambud <jepuerchambudp@utn.edu.ec>
2024-07-01 17:26:59 -0500

    conexion supabase2

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: process.env.DB_USER, // Usuario de la base de datos desde la variable de entorno
   host: process.env.DB_HOST, // Dirección del servidor de la base de datos desde la variable de entorno
   database: process.env.DB_NAME, // Nombre de la base de datos desde la variable de entorno
-  password: process.env.DB_PASSWORD, // Contraseña del usuario de la base de datos desde la variable de entorno
+  password: String(process.env.DB_PASSWORD), // Contraseña del usuario de la base de datos desde la variable de entorno
   port: process.env.DB_PORT, // Puerto desde la variable de entorno
 });
 
