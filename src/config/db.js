04421953d99bab7d4068f6dc978eee99d393aef2
RELIC7663 <alexispepinosa28@gmail.com>
2024-07-11 11:12:36 -0500

    db

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres', // Usuario de la base de datos
   host: 'localhost', // Dirección del servidor de la base de datos
-  database: 'api_seguridad', // Nombre de la base de datos
+  database: 'api_sei', // Nombre de la base de datos
   password: 'root', // Contraseña del usuario de la base de datos
   port: 5432, // Puerto por defecto de PostgreSQL
 });
